e20331d7ba9190ddeb8169d0c9792b357aeaa7c7
Tsotne <cotne.ok@gmail.com>
2019-08-16 17:12:07 +0400

    Add user data to articles listing page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_16_091242) do
+ActiveRecord::Schema.define(version: 2019_08_16_124115) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2019_08_16_091242) do
     t.datetime "created_ad"
     t.datetime "updated_ad"
     t.integer "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
